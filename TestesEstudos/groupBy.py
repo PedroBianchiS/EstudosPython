f3336e4167e9b41c1bc0900b85ed0169830eebf6
Bianchi <pedro.stumpf@yahoo.com.br>
2024-10-29 21:30:48 -0300

    Estudos

---
+++ TestesEstudos/groupBy.py
@@ -0,0 +1,17 @@
+from itertools import groupby
+alunos = [
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+]
+
+grupos = groupby(alunos)
+
+for grupo in grupos:
+    print(grupo)
